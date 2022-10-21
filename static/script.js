4ca17bd0c6d2466c24798ff0cad96ffa35b3cbac
Cheryl Qian <cheryl.q731@gmail.com>
2022-10-21 22:28:18 +0000

    deployed and changed styling

--- static/script.js
+++ static/script.js
@@ -1,28 +1,6 @@
-let h1 = $('#home_description h1')
-let h2 = $('#home_description h2')
-let hr = $('hr')
-let h3 = $('#home_description h3')
-let arrow = $('.arrow')
+$( document ).ready(function() {
 
-
-h2.css('display','none');
-h3.css('display','none');
-hr.css('display','none');
-arrow.css('display','none');
-
-
-
-$('#portfolio').hide()
-
-h1.on('animationend', function() {
-  h2.delay(500).fadeIn();
-  hr.delay(1000).fadeIn();
-  h3.delay(1500).fadeIn();
-  arrow.delay(1600).fadeIn();
-  $('#portfolio').delay(1700).fadeIn();
-})
-
-$('.menu_icon').click(function() {
+    $('.menu_icon').click(function() {
   if ($('.mobile_nav').is(':hidden')) {
     $('.mobile_nav').slideDown();
   }
@@ -30,3 +8,6 @@ $('.menu_icon').click(function() {
     $('.mobile_nav').slideUp();
   }
 })
+
+
+});
